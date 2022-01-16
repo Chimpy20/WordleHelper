0798bf970dee7c7e4f5220fe57bb2364d71a4927
Matt Hayward <hayward.m.d@hotmail.co.uk>
2022-01-16 10:31:38 +0000

    Added bootstrap code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 ﻿#include "pch.h"
-
-using namespace winrt;
-using namespace Windows::Foundation;
+#include <mddbootstrap.h>
 
 int main()
 {
-    init_apartment();
-    Uri uri(L"http://aka.ms/cppwinrt");
-    printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+    winrt::init_apartment();
+
+    // Take a dependency on Windows App SDK Stable.
+    const UINT32 majorMinorVersion{ 0x00010000 };
+    PCWSTR versionTag{ L"" };
+    const PACKAGE_VERSION minVersion{};
+
+    const HRESULT hr{ MddBootstrapInitialize( majorMinorVersion, versionTag, minVersion ) };
+
+    // Check the return code. If there is a failure, display the result.
+    if (FAILED( hr ))
+    {
+        wprintf( L"Error 0x%X in MddBootstrapInitialize(0x%08X, %s, %hu.%hu.%hu.%hu)\n",
+            hr, majorMinorVersion, versionTag, minVersion.Major, minVersion.Minor, minVersion.Build, minVersion.Revision );
+        return hr;
+    }
+
+    MddBootstrapShutdown();
+
+    return 0;
 }
