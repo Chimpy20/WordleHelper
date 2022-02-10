6f44c8090ac7335f22c37f5b87a1b3d090586419
Matt Hayward <hayward.m.d@hotmail.co.uk>
2022-02-10 20:52:03 +0000

    Work on interface

--- resource.h
+++ resource.h
@@ -2,18 +2,33 @@
 // Microsoft Visual C++ generated include file.
 // Used by WordleAnalyser.rc
 //
+#define IDEXIT                          3
+#define IDB_RESET                       4
+#define IDB_RESET2                      5
+#define IDB_FILTERSTATE0                6
+#define IDB_FILTERSTATE1                7
+#define IDB_FILTERSTATE2                8
+#define IDB_FILTERSTATE3                9
+#define IDB_FILTERSTATE4                10
 #define IDD_DIALOG1                     101
-#define IDD_DIALOG2                     103
+#define IDI_ICON1                       107
+#define IDB_BLOCKGREEN                  109
 #define IDC_EDIT1                       1001
 #define IDC_OUTPUT                      1001
+#define IDC_LETTER0                     1002
+#define IDC_LETTER2                     1003
+#define IDC_LETTER1                     1004
+#define IDC_LETTER3                     1005
+#define IDC_LETTER4                     1006
+#define IDC_GUESS                       1007
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        110
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
